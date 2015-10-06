2dba7b351263aab7a373a3bf78ae7a30de95d0ff
Simon Prévidente <simon.previdente@free.fr>
2015-10-07 00:05:56 +0200

    Simple bookmarks list

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 var React = require('react');
 
+var Bookmark = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <p class="title">{this.props.bookmark.title}</p>
+        <p class="link">
+          <a href={this.props.bookmark.link} target="_blank">
+            {this.props.bookmark.link}
+          </a>
+        </p>
+      </div>
+    );
+  }
+
+});
+
 var App = React.createClass({
   render: function() {
     return (
       <div>
-      <h1>Hello world</h1>
+        <h1>My bookmarks listn</h1>
+        <Bookmark bookmark={this.props.bookmarks[0]}></Bookmark>
+        <Bookmark bookmark={this.props.bookmarks[1]}></Bookmark>
       </div>
     )
   }
 });
 
-React.render(<App> </App>, document.getElementById('app'));
+var bookmarks = {
+  rows: [
+    {title: 'title 01', link: 'http://bookmark.com/'},
+    {title: 'title 01', link: 'http://bookmark.com/'} ]
+};
+
+React.render(<App bookmarks={bookmarks.rows}></App>, document.getElementById('app'));
